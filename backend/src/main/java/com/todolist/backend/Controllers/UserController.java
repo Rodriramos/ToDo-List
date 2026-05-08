37d320e5f1dd42ccee52e62131ff957115d3444d
Rodriramos <alu0101479958>
2026-05-08 19:29:46 +0100

    AuthController, jwt and security configuration implemented

--- backend/src/main/java/com/todolist/backend/Controllers/UserController.java
+++
@@ -1,5 +0,0 @@
-package com.todolist.backend.Controllers;
-
-public class UserController {
-
-}
--- backend/src/main/java/com/todolist/backend/Security/JwtUtil.java
+++ backend/src/main/java/com/todolist/backend/Security/JwtUtil.java
@@ -2,6 +2,15 @@ package com.todolist.backend.Security;
 
 import org.springframework.stereotype.Component;
 
+import com.todolist.backend.Entities.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 
 @Component
@@ -12,4 +21,39 @@ public class JwtUtil {
     
     @Value("${jwt.expiration}")
     private Long expirationTime;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
