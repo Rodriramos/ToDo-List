b65eea08cce1fa552604614eb6648d8495652d00
Rodriramos <alu0101479958>
2026-05-11 17:17:08 +0100

    Little changes

--- backend/src/main/java/com/todolist/backend/Security/SecurityConfig.java
+++ backend/src/main/java/com/todolist/backend/Security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.todolist.backend.security;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
